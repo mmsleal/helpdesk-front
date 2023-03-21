2ac266239c6b4d5860f2b18a8414913c8ca2351a
mmsleal <mmsleal@gmail.com>
2023-03-21 20:13:16 -0300

    Rota para componente CHAMADO-CREATE

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './auth/auth.guard';
+import { ChamadoCreateComponent } from './components/chamado-create/chamado-create.component';
 import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-list.component';
 import { ClienteCreateComponent } from './components/cliente/tecnico/cliente-create/cliente-create.component';
 import { ClienteDeleteComponent } from './components/cliente/tecnico/cliente-delete/cliente-delete.component';
@@ -33,6 +34,7 @@ const routes: Routes = [
         {path: 'clientes/delete/:id', component: ClienteDeleteComponent },
 
         {path: 'chamados', component: ChamadoListComponent },
+        {path: 'chamados/create', component:ChamadoCreateComponent },
     ]
   }
 ];
