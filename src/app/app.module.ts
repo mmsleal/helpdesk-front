98e5ebb9a05a25f0c79ea630ba8f45bebd06c828
mmsleal <mmsleal@gmail.com>
2023-03-22 11:09:30 -0300

    componente CHAMADO-READ criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -50,6 +50,7 @@ import { ClienteDeleteComponent } from './components/cliente/tecnico/cliente-del
 import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-list.component';
 import { ChamadoCreateComponent } from './components/chamado/chamado-create/chamado-create.component';
 import { ChamadoUpdateComponent } from './components/chamado/chamado-update/chamado-update.component';
+import { ChamadoReadComponent } from './components/chamado/chamado-read/chamado-read.component';
 
 @NgModule({
   declarations: [
@@ -68,7 +69,8 @@ import { ChamadoUpdateComponent } from './components/chamado/chamado-update/cham
     ClienteDeleteComponent,
     ChamadoListComponent,
     ChamadoCreateComponent,
-    ChamadoUpdateComponent
+    ChamadoUpdateComponent,
+    ChamadoReadComponent
 
   ],
   imports: [
