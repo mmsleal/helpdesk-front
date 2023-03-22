5a1f6d5f757131b37f879e56f1a2a60924a5dec9
mmsleal <mmsleal@gmail.com>
2023-03-22 08:45:07 -0300

    Rota para compoenente CHAMADO-UPDATE

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './auth/auth.guard';
 import { ChamadoCreateComponent } from './components/chamado-create/chamado-create.component';
 import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-list.component';
+import { ChamadoUpdateComponent } from './components/chamado/chamado-update/chamado-update.component';
 import { ClienteCreateComponent } from './components/cliente/tecnico/cliente-create/cliente-create.component';
 import { ClienteDeleteComponent } from './components/cliente/tecnico/cliente-delete/cliente-delete.component';
 import { ClienteListComponent } from './components/cliente/tecnico/cliente-list/cliente-list.component';
@@ -14,7 +15,6 @@ import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecn
 import { TecnicoDeleteComponent } from './components/tecnico/tecnico-delete/tecnico-delete.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 import { TecnicoUpdateComponent } from './components/tecnico/tecnico-update/tecnico-update.component';
-
 
 
 const routes: Routes = [
@@ -35,6 +35,7 @@ const routes: Routes = [
 
         {path: 'chamados', component: ChamadoListComponent },
         {path: 'chamados/create', component:ChamadoCreateComponent },
+        {path: 'chamados/update/:id', component:ChamadoUpdateComponent },
     ]
   }
 ];
